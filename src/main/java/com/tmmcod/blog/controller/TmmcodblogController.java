3178861cf92367dacc2d3fa9dde5fa5476208275
tmmscode <thiago.meloms@gmail.com>
2021-05-30 16:40:26 -0300

    editar post

--- src/main/java/com/tmmcod/blog/controller/TmmcodblogController.java
+++ src/main/java/com/tmmcod/blog/controller/TmmcodblogController.java
@@ -1,11 +1,12 @@
 package com.tmmcod.blog.controller;
 
 import com.tmmcod.blog.model.Post;
+import com.tmmcod.blog.repository.BlogRepository;
 import com.tmmcod.blog.service.TmmcodblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -43,6 +44,14 @@ public class TmmcodblogController {
         return "postForm";
     }
 
+
+    @GetMapping ("/postEdit/{id}")
+    public String getEditForm(@PathVariable("id") long id, Model model) {
+        Post post = tmmcodblogService.findById(id);
+        model.addAttribute("post", post);
+        return "postEdit";
+    }
+
     @PostMapping ("/newpost")
     public String savePost (Post post, BindingResult result, RedirectAttributes attributes){
         if (post.getTitulo().isEmpty() || post.getAutor().isEmpty() || post.getTexto().isEmpty()){
@@ -63,10 +72,31 @@ public class TmmcodblogController {
         return "redirect:/blog/posts";
     }
 
-    /* TODO: Criar endpoint para update e delete dos posts
+    @PostMapping("/saveEdit/{id}")
+    public String updatePost (@PathVariable("id") long id, Post post,
+                             BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            post.setId(id);
+            return "postEdit";
+        }
+
+        tmmcodblogService.save(post);
+        return "redirect:/blog/posts";
+    }
+
+    /* TODO: Criar endpoint para delete dos posts
     *   (em andamento)*/
 
 
+
+/*    @PutMapping ("/posts/{id}")
+    public ModelAndView getPostDetails (@PathVariable("id") long id) {
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = tmmcodblogService.findById(id);
+        mv.addObject("post", post);
+        return mv;
+
+    }*/
 
 
 }
